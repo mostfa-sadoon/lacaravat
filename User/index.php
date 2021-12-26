890c86a6ef5e8e73a9fd5b0775b1392fb0010b14
mostfa-sadoon <54408018+mostfa-sadoon@users.noreply.github.com>
2021-12-26 15:50:45 +0200

    push_error

--- User/index.php
+++ User/index.php
@@ -3,9 +3,10 @@ include_once 'session.php';
 // instantiate database 
 // instantiate object table 
 $category = new Category($db);
-$product = new Product($db);
+// $product = new Product($db);
 $page_title="home";
 //functinality of home bage execute in body
+
 $product=$product->readmainproduct();
 include_once "template/user_templet.php";
 //to use sweet alert one time
