9742b2180e2fcb58ceb9f926062584391d07afab
mostfa-sadoon <54408018+mostfa-sadoon@users.noreply.github.com>
2021-12-26 16:18:16 +0200

    test

--- User/objects/User.php
+++ User/objects/User.php
@@ -73,7 +73,7 @@
             echo "Fdfd";
           
               print_r($this->conn->errorInfo());
-              $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+          echo    $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
           
         }else{
           echo "zepy";
