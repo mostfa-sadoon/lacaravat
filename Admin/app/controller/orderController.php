3fc7bcd16be511970c925e520b9cd66d9fba8108
mostfa-sadoon <54408018+mostfa-sadoon@users.noreply.github.com>
2021-12-30 19:31:07 +0200

    test

--- Admin/app/controller/orderController.php
+++ Admin/app/controller/orderController.php
@@ -6,7 +6,7 @@ if(isset($_GET['page_title']) == "order_action" && isset($_GET['order_id']))
     $order->id=$_GET['order_id'];
     $Orderdetailes= new Orderdetailes($db);
     $Orderdetailes->order_id=$_GET['order_id'];
-    $Orderdetailes=$Orderdetailes->readorderdetailes();
+   $rows=$Orderdetailes->readorderdetailes();
     $order->read();
 }
 if(isset($_POST['accepted']))
