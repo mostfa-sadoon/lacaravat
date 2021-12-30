a42946c1b315dcbc48a125d057e9d4a9c50e2640
mostfa-sadoon <54408018+mostfa-sadoon@users.noreply.github.com>
2021-12-30 12:32:01 +0200

    dd

--- User/order.php
+++ User/order.php
@@ -1,22 +1,4 @@
 <?php
-include_once 'session.php';  
-// instantiate database 
-$database = new Database();
-$db = $database->getConnection();
-// instantiate object table 
-$category = new Category($db);
-$product = new Product($db);
-if(isset($_GET['page_title']))
-{
-$page_title=$_GET['page_title'];
-}else{
-$page_title="order";
-}
-$Order = new Order($db);
-$Order->user_id=$_SESSION['id'];
-$Order->id=$_GET['id'];
-$order=$Order->read();
-$products= $Order->orderproduct();
-include_once "template/user_templet.php";
-$_SESSION['success']="";
+
+echo "Gf";
 ?>
